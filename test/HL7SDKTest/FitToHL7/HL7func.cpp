fe64b836a470b21a68f04cf11c29ac0ba8280736
aurigainc <sergey.ivin@auriga.com>
2016-12-02 16:34:44 +0300

    Corrections of Library name

--- test/HL7SDKTest/FitToHL7/HL7func.cpp
+++ test/HL7SDKTest/FitToHL7/HL7func.cpp
@@ -1,18 +1,18 @@
 /*
- * This file is part of AurigaHL7.
+ * This file is part of Auriga HL7 library.
  *
- * AurigaHL7 is free software: you can redistribute it and/or modify
+ * Auriga HL7 library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * AurigaHL7 is distributed in the hope that it will be useful,
+ * Auriga HL7 library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AurigaHL7.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Auriga HL7 library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 //#include "hl7socket.h"
