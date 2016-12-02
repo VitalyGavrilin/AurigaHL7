c9a31e567c2a17492a5fa74c87e40c0ce9d0b955
aurigainc <sergey.ivin@auriga.com>
2016-12-02 16:24:06 +0300

    Corrected name

--- test/HL7SDKTest/createMes/createMes.cpp
+++ test/HL7SDKTest/createMes/createMes.cpp
@@ -1,18 +1,18 @@
 /*
- * This file is part of HCPPL.
+ * This file is part of AurigaHL7.
  *
- * HCPPL is free software: you can redistribute it and/or modify
+ * AurigaHL7 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * HCPPL is distributed in the hope that it will be useful,
+ * AurigaHL7 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with HCPPL.  If not, see <http://www.gnu.org/licenses/>.
+ * along with AurigaHL7.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // createMes.cpp : Defines the entry point for the console application.
